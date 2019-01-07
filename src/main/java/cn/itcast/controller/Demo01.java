3dcd2c9412a37d0d5d47b7b6f5db7e60efbab463
LQJ <lqj0704@163.com>
2019-01-07 16:12:17 +0800

    4444

--- src/main/java/cn/itcast/controller/Demo01.java
+++ src/main/java/cn/itcast/controller/Demo01.java
@@ -3,7 +3,5 @@ package cn.itcast.controller;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println(1111);
-        System.out.println(2222);
-        System.out.println(33333);
     }
 }
